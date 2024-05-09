42d6e9ffa98711ae019705a0d673920427461ce0
Nolane DELILLE <nolane.de@gmail.com>
2024-05-09 15:35:54 +0100

    bad order on struct

--- ST7735.cpp
+++ ST7735.cpp
@@ -123,8 +123,9 @@ void ST7735::writeData(uint8_t data)
         .tx_buf = (unsigned long)tx,
         .rx_buf = (unsigned long)rx,
         .len = 1,
-        .delay_usecs = 0,
         .speed_hz = this->_spi_speed,
+        .delay_usecs = 0,
+
         .bits_per_word = this->_spi_bits_per_word,
     };
     // set DC pin high for data :
